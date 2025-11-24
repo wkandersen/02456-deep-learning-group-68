fc11e5b805da6d4c4efb1228a679c156b5e8dc67
kris907j <s224197@dtu.dk>
2025-11-24 15:26:15 +0100

    corrected loss

--- src/loss_function.py
+++ src/loss_function.py
@@ -13,13 +13,16 @@ class loss_function:
         return (prediction - target) ** 2
 
     def compute_MSE_loss(self, predictions, targets, weights=None):
-        loss = ((predictions[0] - targets[0]) ** 2).mean()
+        # Mean squared error over all samples
+        loss = ((predictions - targets) ** 2).mean()
 
+        # Optional L2 regularization
         if self.use_regularization and weights is not None:
             l2_norm = self.l2_norm(weights)
             loss += self.lam * l2_norm
 
         return loss
+
     
     def compute_cross_entropy_loss(self, predictions_prob, targets, weights=None):
         n = targets.shape[0]
@@ -27,7 +30,7 @@ class loss_function:
         y_ij = np.zeros((n, k))
         y_ij[np.arange(n), targets] = 1
         yh_ij = predictions_prob
-        loss = -np.sum(y_ij * np.log(yh_ij + self.eps))
+        loss = -np.sum(y_ij * np.log(yh_ij + self.eps))/n
 
         if self.use_regularization and weights is not None:
             l2_norm = self.l2_norm(weights)
